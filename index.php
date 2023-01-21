263a2a3465851ba6ed380f029a269abf80df0490
davipassosppjur <122923434+davipassosppjur@users.noreply.github.com>
2023-01-21 07:25:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo 'Olá Mundo'; ?>
+<html>
+ <head>
+  <title>Curso PHP Progressivo</title>
+ </head>
+ <body>
+  Olá mundo! To no HTML! <br/> 
+  <?php
+   echo "Hello, World! To no PHP!";
+  ?>
+ </body>
+</html>
